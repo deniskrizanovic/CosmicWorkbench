4a62ab1ef5e0a5651380e75e4ae62a02eade8307
denis krizanovic <denis.krizanovic@gmail.com>
2015-08-22 19:08:36 +1000

    Found this rest endpoint.. that now uses the repository too. And respects only current version logic

--- src/main/java/com/fp/web/FunctionPointController.java
+++ src/main/java/com/fp/web/FunctionPointController.java
@@ -1,49 +1,35 @@
 package com.fp.web;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.sql.DataSource;
-
+import com.fp.dao.Repository;
+import com.fp.domain.SystemContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-
-import com.fp.domain.SystemContext;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 public class FunctionPointController {
 
-	private JdbcTemplate jdbcTemplate;
+	private Repository repository;
+
 
 	@Autowired
-	public void setDataSource(DataSource dataSource) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	public void setRepository(Repository repository)
+	{
+		this.repository = repository;
 	}
+
 
 	@RequestMapping("/index/rest")
 	public List<SystemContext> showSystemContext(Model model, HttpServletRequest request) {
 
-		List<SystemContext> actors = this.jdbcTemplate
-				.query("select systemcontextid, version, name, notes, diagram from systemcontext where not deleteflag",
-						new RowMapper<SystemContext>() {
-							public SystemContext mapRow(ResultSet rs, int rowNum)
-									throws SQLException {
-								SystemContext actor = new SystemContext();
-								actor.setName(rs.getString("name"));
-								actor.setNotes(rs.getString("notes"));
-								return actor;
-							}
-						});
+		List<SystemContext> systemContextList = repository.getSystemContexts();
 
-		model.addAttribute("systemcontextlist", actors);
+		model.addAttribute("systemcontextlist", systemContextList);
 
-		return actors;
+		return systemContextList;
 	}
 }
